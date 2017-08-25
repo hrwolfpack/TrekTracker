113e3abb6cd2c9599b47597793dbd8a86f05d666
Kevin Huang <kevinzhuang10@gmail.com>
2017-08-24 18:29:24 -0700

    Refactor Post to single component

---
+++ client/src/components/Post.jsx
@@ -0,0 +1,84 @@
+import React from 'react'
+import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card';
+import Chip from 'material-ui/Chip'
+import FlatButton from 'material-ui/FlatButton';
+import time from '../helpers/time.js';
+import axios from 'axios';
+
+const styles = {
+  chip: {
+    margin: 4,
+  },
+  wrapper: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+};
+
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleLike = this.handleLike.bind(this);
+    this.handleUnlike = this.handleUnlike.bind(this);
+  }
+
+  componentDidMount() {
+    //get all the liked posts from user
+    
+  }
+
+  handleLike() {
+    axios.post('/api/posts/like', {postId: this.props.post.id})
+    .then(result => {
+      console.log('Liked this post!');
+    });
+  }
+
+  handleUnlike() {
+    axios.post('/api/posts/unlike', {postId: this.props.post.id})
+    .then(result => {
+      console.log('Unliked this post!');
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Card className='post'>
+          <CardHeader
+            title={this.props.post.poster.firstname + ' ' + this.props.post.poster.lastname}
+            subtitle={this.props.post.poster.email}
+          />
+          <CardMedia 
+          overlay={<CardTitle title={this.props.post.text} 
+          subtitle={time.parse(this.props.post.createdAt, true)} />}>
+            <img src={this.props.post['image_url']}/>
+          </CardMedia>
+          <div style={styles.wrapper}>
+            <Chip style={styles.chip}>Mountain</Chip>
+            <Chip style={styles.chip}>Lake</Chip>
+            <Chip style={styles.chip}>Tree</Chip>
+          </div>
+          <div>
+            <FlatButton 
+            label='Like' 
+            primary={true} 
+            secondary={true}
+            onClick={this.handleLike}/>
+            <FlatButton 
+            label='Unlike' 
+            primary={false} 
+            secondary={true}
+            onClick={this.handleUnlike}/>
+            <FlatButton 
+            label='See More Posts About This Trail' 
+            primary={true}
+            onClick={() => {window.location.href = '/trail?id=' + this.props.post.trail_id}}/>
+          </div>
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default Post;
--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -1,42 +1,12 @@
 import React from 'react'
-import { Card, CardActions, CardHeader, CardMedia, CardTitle, CardText } from 'material-ui/Card'
-import FlatButton from 'material-ui/FlatButton';
-import time from '../helpers/time.js';
-import axios from 'axios';
+import Post from './Post.jsx';
 
-class Posts extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleLike = this.handleLike.bind(this);
-  }
-
-  componentDidMount() {
-    //get all the liked posts from user
-  }
-
-  handleLike(postId) {
-    axios.post('/api/posts/like', {postId})
-    .then(result => {
-      console.log('Liked this post!');
-    });
-  }
-
-  handleUnlike(postId) {
-    axios.post('/api/posts/unlike', {postId})
-    .then(result => {
-      console.log('Unliked this post!');
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        {this.props.posts.map((post, i) => (
-          <Post post={post} key={i} />
-        ))}
-      </div>
-    );
-  }
-}
+var Posts = (props) => (
+  <div>
+    {props.posts.map((post, i) => (
+      <Post post={post} key={i} />
+    ))}
+  </div>
+);
 
 export default Posts;
