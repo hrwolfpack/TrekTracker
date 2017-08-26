17cd367abe101d56b395fd85c8e9036f77dfefb6
Kevin Huang <kevinzhuang10@gmail.com>
2017-08-26 16:15:33 -0700

    Add service account credential

--- server/foreign-apis/google-cloud-vision.js
+++ server/foreign-apis/google-cloud-vision.js
@@ -3,8 +3,30 @@ const path = require('path');
 // Imports the Google Cloud client library
 const Vision = require('@google-cloud/vision');
 
+const vision;
+if (process.env.NODE_ENV === 'development') {
+  vision = Vision();
+} else {
+  vision = Vision({
+    credentials: {
+      "type": process.env.type,
+      "project_id": process.env.project_id,
+      "private_key_id": process.env.private_key_id,
+      "private_key": process.env.private_key,
+      "client_email": process.env.client_email,
+      "client_id": process.env.client_id,
+      "auth_uri": process.env.auth_uri,
+      "token_uri": process.env.token_uri,
+      "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
+      "client_x509_cert_url": process.env.client_x509_cert_url
+    }
+  });
+}
+
 // Instantiates a client
-const vision = Vision();
+// const vision = Vision({
+//   keyFilename: path.join(__dirname, '../../GAC.config.json')
+// });
 
 // The name of the image file to annotate
 // const fileName = path.join(__dirname, 'bigsur.jpg');
