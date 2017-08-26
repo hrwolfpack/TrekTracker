1b80d4e0412702ec45976b36a77ef4074ae06eec
Kevin Huang <kevinzhuang10@gmail.com>
2017-08-26 16:58:58 -0700

    Change cred for google vision

--- server/foreign-apis/google-cloud-vision.js
+++ server/foreign-apis/google-cloud-vision.js
@@ -3,22 +3,22 @@ const path = require('path');
 // Imports the Google Cloud client library
 const Vision = require('@google-cloud/vision');
 
-const vision;
-if (process.env.NODE_ENV === 'development') {
-  vision = Vision();
+var env = process.env.NODE_ENV || 'development';
+if (env === 'development') {
+  var vision = Vision();
 } else {
-  vision = Vision({
+  var vision = Vision({
     credentials: {
-      "type": process.env.type,
-      "project_id": process.env.project_id,
-      "private_key_id": process.env.private_key_id,
-      "private_key": process.env.private_key,
-      "client_email": process.env.client_email,
-      "client_id": process.env.client_id,
-      "auth_uri": process.env.auth_uri,
-      "token_uri": process.env.token_uri,
-      "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url,
-      "client_x509_cert_url": process.env.client_x509_cert_url
+      "type": process.env.type || null,
+      "project_id": process.env.project_id || null,
+      "private_key_id": process.env.private_key_id || null,
+      "private_key": process.env.private_key || null,
+      "client_email": process.env.client_email || null,
+      "client_id": process.env.client_id || null,
+      "auth_uri": process.env.auth_uri || null,
+      "token_uri": process.env.token_uri || null,
+      "auth_provider_x509_cert_url": process.env.auth_provider_x509_cert_url || null,
+      "client_x509_cert_url": process.env.client_x509_cert_url || null
     }
   });
 }
